7891a68d57eb1fc8d821c141619c8be0652a2fa9
YooSunJong <jobgsyou@naver.com>
2021-04-28 00:48:14 +0900

    Update DP

---
+++ BOJ/DP/Plus123.py
@@ -0,0 +1,17 @@
+# 1, 2, 3 더하기
+
+
+# bottom-up
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    dp = [0]*(n+1)
+    dp[1] = 1
+    if n > 1:
+        dp[2] = 2
+    if n > 2:
+        dp[3] = 4
+    if n > 3:
+        for i in range(4, n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[n])
